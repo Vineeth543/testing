5a0426dd4e47f1ea8058df01c9ae489b5b345925
Vineeth Serigar <vineeth.serigar@netcracker.com>
2024-03-15 00:26:34 +0530

    Initial Successfull Testing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [provideRouter(routes), importProvidersFrom(BrowserModule), provideAnimations()],
